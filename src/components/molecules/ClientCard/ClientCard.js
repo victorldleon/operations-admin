2eced89069f1b651094f09459b6ae369aa31ef01
Victor De León <victorldleon@gmail.com>
2022-05-11 16:55:41 -0600

    adds correct info to client card

--- src/components/molecules/ClientCard/ClientCard.js
+++ src/components/molecules/ClientCard/ClientCard.js
@@ -9,7 +9,10 @@ const ClientCard = () => {
                 <h5 className="name">Hitrecord</h5>
             </div>
             <div className="right-content">
-                <p className="english-level">
+                <p className="detail">
+                   Responsable: <b>Victor De Leon</b>
+                </p>
+                <p className="detail">
                     Members: <span>9</span>
                 </p>
                 <a href="../../../logo.svg" className="cv-link" download>Manage</a>
