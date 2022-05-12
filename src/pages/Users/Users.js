9f38567c2e6be2736358b50193ca909ca5a61a20
Victor De León <victorldleon@gmail.com>
2022-05-12 09:51:15 -0600

    adds new user button

--- src/pages/Users/Users.js
+++ src/pages/Users/Users.js
@@ -1,8 +1,18 @@
+import { Link } from "react-router-dom";
 import UsersList from "../../components/organisms/UsersList/UserList";
+import './Users.css';
 function UsersPage() {
     return (
         <div>
-            <h1>Users</h1>
+            <div className="sub-header">
+                <h1>Users</h1>
+                <Link to="/users/new"className="row" >
+                    <svg className="icon" viewBox="0 0 24 24">
+                        <path d="M19,13H13V19H11V13H5V11H11V5H13V11H19V13Z" fill="#fff" />
+                    </svg>
+                <p>New User</p>
+                </Link>
+            </div>
             <UsersList />
         </div>
     );
