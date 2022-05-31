24658f6e409bb94b64d2ea12b60ab82aaee38ef0
Victor De León <victorldleon@gmail.com>
2022-05-31 12:10:35 -0600

    test 1

--- src/pages/Clients/Clients.js
+++ src/pages/Clients/Clients.js
@@ -11,7 +11,7 @@ const ClientsPage = () => {
                     <svg className="icon" viewBox="0 0 24 24">
                         <path d="M19,13H13V19H11V13H5V11H11V5H13V11H19V13Z" fill="#fff" />
                     </svg>
-                <p>New Client</p>
+                <p>New Client 1</p>
                 </Link>
             </div>
             <ClientsList />
